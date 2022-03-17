14fdca2a49c5399f1580e032ee80511d31e9a44d
GoninM <goninmihon@yandex.ru>
2022-03-17 05:24:15 +0300

    lesson updated

--- lesson.py
+++ lesson.py
@@ -14,10 +14,14 @@ def fib_dict(n):
     print(f' in: n= {n} {fib_d}')
 
     def _fib_dict(n):
+        print(f'func in n= {n} {fib_d}')
+
         if n in fib_d:
             return fib_d[n]
 
         fib_d[n] = _fib_dict(n-1) + _fib_dict(n-2)
+
+        print(f'func out {fib_d}')
         return fib_d[n]
 
     print(f'out {fib_d}')
