1eb5c21be81507d12fcff574490a0a5cc5b1ef4a
GoninM <goninmihon@yandex.ru>
2022-03-17 05:18:48 +0300

    added task_1 task_2 lesson

--- task_1.py
+++ task_1.py
@@ -5,3 +5,51 @@
 # c. проанализировать 3 варианта и выбрать оптимальный,
 # d. результаты анализа вставить в виде комментариев в файл с кодом (не забудьте указать, для каких N вы проводили замеры),
 # e. написать общий вывод: какой из трёх вариантов лучше и почему.
+
+
+# 5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+# Примечание к задаче: пожалуйста не путайте «минимальный» и «максимальный отрицательный».
+# Это два абсолютно разных значения.
+import random
+import cProfile
+
+
+def find_index_max_negative_element(input_list):
+    index = -1
+
+    for item in input_list:
+        if item < 0 and index == -1:
+            index = input_list.index(item)
+        elif item < 0:
+            if abs(item) < abs(input_list[index]):
+                index = input_list.index(item)
+        else:
+            pass
+
+    return index
+
+
+# def main():
+test_data_1 = [random.randint(-100, 100) for _ in range(1000)]
+find_index_max_negative_element(test_data_1)
+
+
+# cProfile.run('main()')
+
+# n = 100
+# 628 function calls in 0.000 seconds
+# 1    0.000    0.000    0.000    0.000 task_1.py:17(find_index_max_negative_element)
+
+# n = 500
+# 3158 function calls in 0.002 seconds
+# 1    0.000    0.000    0.000    0.000 task_1.py:17(find_index_max_negative_element)
+
+# n = 1000
+# 6220 function calls in 0.003 seconds
+# 1    0.000    0.000    0.000    0.000 task_1.py:17(find_index_max_negative_element)
+# 926    0.000    0.000    0.000    0.000 {built-in method builtins.abs}
+
+# 100 loops, best of 5: 9 nsec per loop
+# 200 loops, best of 5: 10 nsec per loop
+# 1000 loops, best of 5: 11.2 nsec per loop
+# 10000 loops, best of 5: 10.6 nsec per loop
--- task_2.py
+++ task_2.py
@@ -15,4 +15,202 @@
 # 11
 # >>> prime(1)
 # 2
+import cProfile
+import functools
 
+
+def test(func):
+    lst = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for i, item in enumerate(lst):
+        assert item == func(i+1)
+        print(f'Test passed. element №{i+1} is {lst[i]}')
+
+
+def erastofens_sieve(n):
+    sieve = [i for i in range(n)]
+    sieve[1] = 0
+
+    for i in range(2, n):
+        if sieve[i] != 0:
+            j = 2 * i
+
+            while j < n:
+                sieve[j] = 0
+                j += i
+
+    result = [i for i in sieve if i != 0]
+
+    return result
+
+
+def erastofen(n):
+    result = [2]
+
+    if n <= len(result):
+        return result[n-1]
+    else:
+        number_to_check = result[-1] + 10000
+        while len(result) < n:
+            sieve = [i for i in range(number_to_check)]
+            sieve[1] = 0
+
+            for i in range(2, number_to_check):
+                if sieve[i] != 0:
+                    j = 2 * i
+
+                    while j < number_to_check:
+                        sieve[j] = 0
+                        j += i
+
+            result = [i for i in sieve if i != 0]
+
+    return result[n-1]
+
+
+def simple(n):
+    result = [2]
+
+    if n <= len(result):
+        return result[n-1]
+    else:
+        number_to_check = result[-1] + 1
+        index = 0
+
+        while len(result) < n:
+            if number_to_check % result[index] == 0 and index < len(result) - 1:
+                number_to_check += 1
+                index = 0
+            else:
+                index += 1
+
+                if index >= len(result):
+                    result.append(number_to_check)
+                    number_to_check += 1
+                    index = 0
+
+        return result[n-1]
+
+
+# def simple_dict(n):
+#     dict = {1: 2}
+#
+#     def _simple_dict(n):
+#         if n in dict:
+#             return dict[n]
+#         else:
+#             keys = list(dict.keys())
+#             print(keys)
+#             print(keys[-1])
+#             last_index = keys[-1]
+#             last_value = dict[last_index]
+#             number_to_check = last_value + 1
+#             index = 0
+#             while len(dict) < n:
+#                 # print(f'loop: index = {index}, number check = {number_to_check}, {dict}')
+#                 if number_to_check % dict[keys[index]] == 0 and index < len(keys) - 1:
+#                     number_to_check += 1
+#                     index = 0
+#                 else:
+#                     index += 1
+#
+#                     if index >= len(keys):
+#                         dict[keys[index-1]+1] = number_to_check
+#                         # result.append(number_to_check)
+#                         number_to_check += 1
+#                         index = 0
+#                         keys = list(dict.keys())
+#
+#         return dict[n]
+#             # pass
+#
+#     return _simple_dict(n)
+
+
+# print(erastofens_sieve(100))
+
+# print(f'simple')
+# test(simple)
+# "task_2.simple(10)"
+# 1000 loops, best of 5: 22 usec per loop
+
+# "task_2.simple(100)"
+# 1000 loops, best of 5: 1.47 msec per loop
+
+# "task_2.simple(500)"
+# 1000 loops, best of 5: 46 msec per loop
+
+
+# print(f'erastofen')
+# test(erastofen)
+
+# task_2.erastofen(10)
+# 1000 loops, best of 5: 442 usec per loop
+
+# task_2.erastofen(100)
+# 1000 loops, best of 5: 403 usec per loop
+
+# task_2.erastofen(500)
+# 1000 loops, best of 5: 4.94 msec per loop
+
+# cProfile.run("simple(10)")
+#  153 function calls in 0.000 seconds
+# ncalls tottime percall cumtime  percall filename: lineno(function)
+# 1 0.000 0.000  0.000 0.000 < string >: 1( < module >)
+# 1 0.000 0.000 0.000 0.000 task_2.py: 69(simple)
+# 1 0.000 0.000 0.000 0.000 {built - in method builtins.exec}
+# 140 0.000 0.000 0.000 0.000 {built - in method builtins.len}
+# 9 0.000 0.000 0.000 0.000 {method 'append' of 'list' objects}
+# 1 0.000 0.000 0.000 0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# cProfile.run("simple(100)")
+# 11595 function calls in 0.004 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+# 1    0.000    0.000    0.004    0.004 <string>:1(<module>)
+# 1    0.003    0.003    0.004    0.004 task_2.py:69(simple)
+# 1    0.000    0.000    0.004    0.004 {built-in method builtins.exec}
+# 11492    0.001    0.000    0.001    0.000 {built-in method builtins.len}
+# 99    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+# 1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# cProfile.run("simple(500)")
+# 263597 function calls in 0.140 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+# 1    0.000    0.000    0.140    0.140 <string>:1(<module>)
+# 1    0.104    0.104    0.140    0.140 task_2.py:69(simple)
+# 1    0.000    0.000    0.140    0.140 {built-in method builtins.exec}
+# 263094    0.036    0.000    0.036    0.000 {built-in method builtins.len}
+# 499    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+# 1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# cProfile.run("erastofen(10)")
+# 9 function calls in 0.005 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+# 1    0.000    0.000    0.005    0.005 <string>:1(<module>)
+# 1    0.004    0.004    0.005    0.005 task_2.py:45(erastofen)
+# 1    0.000    0.000    0.000    0.000 task_2.py:53(<listcomp>)
+# 1    0.001    0.001    0.001    0.001 task_2.py:64(<listcomp>)
+# 1    0.000    0.000    0.005    0.005 {built-in method builtins.exec}
+# 3    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+# 1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# cProfile.run("erastofen(100)")
+# 9 function calls in 0.005 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+# 1    0.000    0.000    0.005    0.005 <string>:1(<module>)
+# 1    0.004    0.004    0.004    0.004 task_2.py:45(erastofen)
+# 1    0.000    0.000    0.000    0.000 task_2.py:53(<listcomp>)
+# 1    0.000    0.000    0.000    0.000 task_2.py:64(<listcomp>)
+# 1    0.000    0.000    0.005    0.005 {built-in method builtins.exec}
+# 3    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+# 1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# cProfile.run("erastofen(500)")
+# 9 function calls in 0.004 seconds
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+# 1    0.000    0.000    0.004    0.004 <string>:1(<module>)
+# 1    0.003    0.003    0.004    0.004 task_2.py:45(erastofen)
+# 1    0.000    0.000    0.000    0.000 task_2.py:53(<listcomp>)
+# 1    0.000    0.000    0.000    0.000 task_2.py:64(<listcomp>)
+# 1    0.000    0.000    0.004    0.004 {built-in method builtins.exec}
+# 3    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+# 1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
